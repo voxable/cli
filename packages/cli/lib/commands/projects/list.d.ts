16bd54d86b51e146de9043f5a8eab68440597a5e
Matt Buck <matt@voxable.io>
2021-05-18 12:16:18 -0400

    Drop compiled files

--- packages/cli/lib/commands/projects/list.d.ts
+++
@@ -1,9 +0,0 @@
-import Base from '@voxable/command';
-export default class ListProjects extends Base {
-    static description: string;
-    static examples: string[];
-    static flags: {
-        json: import("@oclif/parser/lib/flags").IBooleanFlag<boolean>;
-    };
-    run(): Promise<undefined>;
-}
--- packages/command/lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import Command from '@oclif/command';
-import Client from '@voxable/client';
-export default abstract class extends Command {
-    private configuration;
-    protected client: Client | undefined;
-    static flags: Record<string, any>;
-    init(): Promise<void>;
-}
